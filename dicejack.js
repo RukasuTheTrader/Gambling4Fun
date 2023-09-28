f44afd5f23d08d9eb6b0cfc9f708c6ff7048ef1f
RukasuTheTrader <145609815+RukasuTheTrader@users.noreply.github.com>
2023-09-28 23:38:28 +0200

    Update dicejack.js

--- dicejack.js
+++ dicejack.js
@@ -1,70 +1,87 @@
-let jackAmount = 1000;
-let playerPoints = 0;
-let hostPoints = 0;
-let betAmount = 10;
-const jackAmountParagraph = document.getElementById('jack-amount');
-const playerContainer = document.getElementById('player-container');
-const hostContainer = document.getElementById('host-container');
-const endGameButtons = document.getElementById('end-game-buttons');
-const startButton = document.getElementById('start-button');
-const rollButton = document.getElementById('roll-button');
-const stopButton = document.getElementById('stop-button');
-const newGameButton = document.getElementById('new-game-button');
-const exitButton = document.getElementById('exit-button');
-const playerPointsSpan = document.getElementById('player-points');
-const hostPointsSpan = document.getElementById('host-points');
-const resultParagraph = document.getElementById('result');
-const betInput = document.getElementById('bet-input');
+let playerJack = 1000; // Spieler startet mit 1000 Jack
+let playerSum = 0;
+let hostSum = 0;
 
-startButton.addEventListener('click', startGame);
-rollButton.addEventListener('click', playerRoll);
-stopButton.addEventListener('click', hostRoll);
-newGameButton.addEventListener('click', newGame);
-exitButton.addEventListener('click', exitGame);
+// Beispiel-Elemente, ersetzen Sie diese mit Ihren tatsächlichen Element-Referenzen
+let playerSumElement = document.getElementById('playerSum');
+let hostSumElement = document.getElementById('hostSum');
+let playerJackElement = document.getElementById('playerJack');
+let rollButton = document.getElementById('rollButton');
+let holdButton = document.getElementById('holdButton');
+let newGameButton = document.getElementById('newGameButton');
+let exitButton = document.getElementById('exitButton');
+let betInput = document.getElementById('betInput');
 
 function startGame() {
-    betAmount = parseInt(betInput.value);
-    if (betAmount < 10 || betAmount > jackAmount) return;
-    jackAmount -= betAmount;
-    jackAmountParagraph.textContent = `Jack: ${jackAmount}`;
-    playerContainer.classList.remove('hidden');
-    hostContainer.classList.remove('hidden');
-    startButton.classList.add('hidden');
-    betInput.classList.add('hidden');
-}
-
-function playerRoll() {
-    const roll = rollDice();
-    if (roll <= 4) {
-        endGame('Host gewinnt!', false);
+    let bet = parseInt(betInput.value);
+    if(isNaN(bet) || bet < 10 || bet > playerJack) {
+        alert('Invalid bet amount');
         return;
     }
-    playerPoints += roll;
-    playerPointsSpan.textContent = playerPoints;
-    if (playerPoints > 100) {
-        endGame('Host gewinnt!', false);
-    }
-}
 
-function hostRoll() {
-    while (hostPoints < playerPoints && hostPoints <= 100) {
-        const roll = rollDice();
-        hostPoints += roll;
-        hostPointsSpan.textContent = hostPoints;
-    }
-    if (hostPoints >= playerPoints && hostPoints <= 100) {
-        endGame('Unentschieden!', null);
-    } else {
-        endGame('Spieler gewinnt!', true);
-    }
+    playerJack -= bet;
+    playerJackElement.textContent = playerJack;
+    playerSum = 0;
+    hostSum = 0;
+    rollButton.disabled = false;
+    holdButton.disabled = false;
+    newGameButton.disabled = true;
+    exitButton.disabled = true;
 }
 
 function rollDice() {
-    return Math.floor(Math.random() * 100) + 1;
+    let roll = Math.floor(Math.random() * 100) + 1;
+    playerSum += roll;
+    playerSumElement.textContent = playerSum;
+
+    if(roll <= 4 || playerSum > 100) {
+        endGame('Host wins!', false);
+    }
+}
+
+function hold() {
+    rollButton.disabled = true;
+    holdButton.disabled = true;
+    
+    while(hostSum < playerSum && hostSum <= 100) {
+        let roll = Math.floor(Math.random() * 100) + 1;
+        hostSum += roll;
+        hostSumElement.textContent = hostSum;
+    }
+
+    if(hostSum <= 4 || hostSum > 100) {
+        endGame('Player wins!', true);
+    } else if(hostSum >= playerSum && hostSum <= 100) {
+        endGame('Host wins!', false);
+    } else {
+        endGame('Player wins!', true);
+    }
 }
 
 function endGame(message, playerWins) {
-    rollButton.disabled = true;
-    stopButton.disabled = true;
-    resultParagraph.textContent = message;
-    end
+    alert(message);
+    if(playerWins) playerJack += parseInt(betInput.value) * 2;
+    playerJackElement.textContent = playerJack;
+    newGameButton.disabled = false;
+    exitButton.disabled = false;
+}
+
+function newGame() {
+    playerSum = 0;
+    hostSum = 0;
+    playerSumElement.textContent = playerSum;
+    hostSumElement.textContent = hostSum;
+    startGame();
+}
+
+function exitGame() {
+    // Hier Logik zum Verlassen des Spiels oder Zurückkehren zum Hauptmenü
+    window.location.href = 'index.html'; // Beispiel: Leitet zur Startseite weiter.
+}
+
+rollButton.addEventListener('click', rollDice);
+holdButton.addEventListener('click', hold);
+newGameButton.addEventListener('click', newGame);
+exitButton.addEventListener('click', exitGame);
+
+startGame(); // Startet das Spiel beim Laden der Seite
